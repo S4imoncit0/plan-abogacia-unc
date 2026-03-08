78477ddc57b9f7b467fd8469dd4a15ce2aa92cbf
Saimon <saimon2594@gmail.com>
2026-03-07 23:35:12 -0300

    Update materias.js

--- data/materias.js
+++ data/materias.js
@@ -1,29 +1,31 @@
 const materias = [
 
-{ id:"intro_derecho", nombre:"Introducción al Derecho" },
-{ id:"romano", nombre:"Derecho Romano" },
-{ id:"historia", nombre:"Historia Constitucional" },
-{ id:"sociologia", nombre:"Sociología" },
+{
+id:"intro_derecho",
+nombre:"Introducción al Derecho",
+x:100,
+y:100
+},
 
-{ id:"privado", nombre:"Derecho Privado Parte General" },
+{
+id:"romano",
+nombre:"Derecho Romano",
+x:350,
+y:100
+},
 
-{ id:"penal1", nombre:"Derecho Penal I" },
-{ id:"civil1", nombre:"Derecho Civil I" },
+{
+id:"historia",
+nombre:"Historia Constitucional",
+x:600,
+y:100
+},
 
-{ id:"civil2", nombre:"Derecho Civil II" },
-{ id:"penal2", nombre:"Derecho Penal II" }
-
-]
-
-const correlativas = [
-
-{ source:"intro_derecho", target:"privado" },
-{ source:"romano", target:"privado" },
-
-{ source:"privado", target:"civil1" },
-{ source:"privado", target:"penal1" },
-
-{ source:"civil1", target:"civil2" },
-{ source:"penal1", target:"penal2" }
+{
+id:"privado",
+nombre:"Derecho Privado",
+x:350,
+y:250
+}
 
 ]
